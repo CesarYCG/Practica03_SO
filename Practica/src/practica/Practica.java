a2e3cec54b5ff9592491dd99bc7f3a66c501cdf3
Fernando1612 <fernando.patricio.maceda@gmail.com>
2021-10-12 16:28:34 -0500

    add menu

--- Practica/src/practica/Practica.java
+++ Practica/src/practica/Practica.java
@@ -4,6 +4,9 @@
  */
 package practica;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author fernando
@@ -14,7 +17,59 @@ public class Practica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Crear Proceso nuevo");
+            System.out.println("2. Ver estado actual del sistema");
+            System.out.println("3. Imprimir cola de procesos");
+            System.out.println("4. Ver proceso actual");
+            System.out.println("5. Ejecutar proceso actual");
+            System.out.println("6. Pasar al proceso siguiente");
+            System.out.println("7. Matar proceso actual");
+            System.out.println("8. Salir del programa");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado la opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        System.out.println("Has seleccionado la opcion 4");
+                        break;
+                    case 5:
+                        System.out.println("Has seleccionado la opcion 5");
+                        break;
+                    case 6:
+                        System.out.println("Has seleccionado la opcion 6");
+                        break;
+                    case 7:
+                        System.out.println("Has seleccionado la opcion 7");
+                        break;
+                    case 8:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
     }
     
 }
